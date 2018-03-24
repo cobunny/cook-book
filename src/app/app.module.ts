ed7284196d60815bb1cf20228e2dee2ba6769a5d
Cobunny <malatanggm@gmail.com>
2018-03-24 15:30:17 -0700

    Header shows up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeDetailComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
