645db0eafe46484ecf9447909b66295a974b65bb
Cobunny <malatanggm@gmail.com>
2018-03-28 21:48:05 -0700

    Added a dropdown button in recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeClicked = new EventEmitter<Recipe>();
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'Hot pot',
       'Very spicy',
-      'https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg'
+      'https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg',
+      [new Ingredient('Bread', 2), new Ingredient('Blueberries', 20)]
     ),
     new Recipe(
       'Beef noodles',
       'This is a test recipe',
-      'https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg'
+      'https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg',
+      [new Ingredient('Basils', 4), new Ingredient('Tomatoes', 1)]
     )
   ];
 
