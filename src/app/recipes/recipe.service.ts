c474c821ef277257db0e0dd2a04d38d4da8a8be9
Cobunny <malatanggm@gmail.com>
2018-03-28 22:53:46 -0700

    Recipe dropdown menu item 'Add to Shopping List' is working

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeClicked = new EventEmitter<Recipe>();
 
@@ -20,7 +22,13 @@ export class RecipeService {
     )
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
