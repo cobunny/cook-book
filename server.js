7add165a3b937e63fc534263a062b29da6d4193a
cobunny <malatanggm@gmail.com>
2018-10-15 18:25:12 -0700

    Force SSL

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
+const secure = require('ssl-express-www');
 
 const app = express();
 
+// Force SSL
+app.use(secure);
 app.use(express.static(__dirname + '/dist'));
 
 app.all('*', (req, res) => {
   res.status(200).sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8070);
